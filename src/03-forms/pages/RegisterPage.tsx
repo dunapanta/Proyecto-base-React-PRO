5ffd7ea46ae30694cbc0e0d46ae466533dd16490
Daniel Unapanta <dunapanta@outlook.es>
2022-10-19 18:59:08 -0500

    Add functionality form

--- src/03-forms/pages/RegisterPage.tsx
+++ src/03-forms/pages/RegisterPage.tsx
@@ -1,17 +1,58 @@
-import '../styles/styles.css'
+import { ChangeEvent, FormEvent, useState } from "react";
+import "../styles/styles.css";
 
 export const RegisterPage = () => {
+  const [registerData, setRegisterData] = useState({
+    name: "",
+    email: "",
+    password1: "",
+    password2: "",
+  });
+  const { name, email, password1, password2 } = registerData;
+
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setRegisterData((prev) => ({
+      ...registerData,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const onSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(registerData);
+  };
+
   return (
     <div>
       <h1>Register Page</h1>
-      <form>
-        <input type="text" placeholder="Name" name="name" />
-        <input type="email" placeholder="Email" name="email" />
-        <input type="password" placeholder="Password" name="password" />
+      <form noValidate onSubmit={onSubmit}>
+        <input
+          type="text"
+          value={name}
+          placeholder="Name"
+          name="name"
+          onChange={onChange}
+        />
+        <input
+          type="email"
+          value={email}
+          placeholder="Email"
+          name="email"
+          onChange={onChange}
+        />
         <input
           type="password"
-          placeholder="Repeat Password"
-          name="repeatPassword"
+          value={password1}
+          placeholder="Password"
+          name="password1"
+          onChange={onChange}
+        />
+        <input
+          type="password"
+          value={password2}
+          placeholder="Confirm Password"
+          name="password2"
+          onChange={onChange}
         />
         <button type="submit">Create</button>
       </form>
