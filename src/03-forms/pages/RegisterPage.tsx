2be3d857752d33fe5707f2ab4bff1b5a051574db
Daniel Unapanta <dunapanta@outlook.es>
2022-10-19 21:10:53 -0500

    Add validation text for inputs

--- src/03-forms/pages/RegisterPage.tsx
+++ src/03-forms/pages/RegisterPage.tsx
@@ -3,7 +3,16 @@ import { useForm } from "../hooks/useForm";
 import "../styles/styles.css";
 
 export const RegisterPage = () => {
-  const { formData, onChange, name, email, password1, password2 } = useForm({
+  const {
+    formData,
+    onChange,
+    name,
+    email,
+    password1,
+    password2,
+    resetForm,
+    isValidEmail,
+  } = useForm({
     name: "",
     email: "",
     password1: "",
@@ -25,14 +34,18 @@ export const RegisterPage = () => {
           placeholder="Name"
           name="name"
           onChange={onChange}
+          className={`${name.trim().length <= 0 && "has-error"}  `}
         />
+        {name.trim().length <= 0 && <span>Este campo es necesario</span>}
         <input
           type="email"
           value={email}
           placeholder="Email"
           name="email"
           onChange={onChange}
+          className={`${!isValidEmail(email) && "has-error"} `}
         />
+        {!isValidEmail(email) && <span>Email no es válido</span>}
         <input
           type="password"
           value={password1}
@@ -40,6 +53,8 @@ export const RegisterPage = () => {
           name="password1"
           onChange={onChange}
         />
+         {password1.trim().length <= 0 && <span>Este campo es necesario</span>}
+         {password1.trim().length < 6 && password1.trim().length > 0 && <span>La contraseña debe tener al menos 6 letras</span>}
         <input
           type="password"
           value={password2}
@@ -47,7 +62,12 @@ export const RegisterPage = () => {
           name="password2"
           onChange={onChange}
         />
+         {password2.trim().length <= 0 && <span>Este campo es necesario</span>}
+         {password2.trim().length > 0 && password1 !== password2 && <span>Las contraseñas no coinciden</span>}
         <button type="submit">Create</button>
+        <button type="button" onClick={resetForm}>
+          Reset
+        </button>
       </form>
     </div>
   );
